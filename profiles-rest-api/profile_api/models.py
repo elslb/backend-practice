21db13124083ae06cf9bf0bf37fa35792c54bc73
Elie Saliba <ersaliba@outlook.com>
2022-10-17 19:18:16 -0400

    Added User Profile Manager to models.py

--- profiles-rest-api/profile_api/models.py
+++ profiles-rest-api/profile_api/models.py
@@ -1,7 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """
+    Manager for user profiles
+    """
+
+    def create_user(self, email, name, password=None):
+        """
+        Create a new user profile
+
+        Because of the way the Django system works a None password
+        will throw an error because passwords need to be a hash
+        so not setting a password will not allow a user to authenticate
+        """
+
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        # set_password function encrypts passwords in Django
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """
+        Create new superuser with the specified details
+        """
+
+        # no need to pass self into a class function when you call it
+        # it will be automatically passed in, this is part of python
+        user = self.create_user(email,name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """
     Database model for users in the system
@@ -39,4 +84,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """
         return self.email
 
-        
