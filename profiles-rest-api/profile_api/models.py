148e13ac51b1770360b09ac86e795ba734c67efd
Elie Saliba <ersaliba@outlook.com>
2022-10-17 19:07:01 -0400

    Added user database model to models.py

--- profiles-rest-api/profile_api/models.py
+++ profiles-rest-api/profile_api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Database model for users in the system
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    # because we are defining a function within a class
+    # we must pass in self as an argument
+    # this is the default python convention
+    def get_full_name(self):
+        """
+        Retrieve full name of user
+        """
+        return self.name
+
+    def get_short_name(self):
+        """
+        Retrieve short name of user
+        """
+        return self.name
+
+    def __str__(self):
+        """
+        Return data from our user cast as a string
+        """
+        return self.email
+
+        
