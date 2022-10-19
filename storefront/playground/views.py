7fd5100b0ee028588189b6041e4b8dc103ea97ef
Elie Saliba <ersaliba@outlook.com>
2022-10-18 22:43:58 -0400

    Added Hello World request to views.py

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# A view functions is a function that takes a request
+# and returns a response
+# this is commonly known as a request handler
+
+def say_hello(request):
+    return HttpResponse('Hello World!')
