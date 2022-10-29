9134f63402bbc999ecfe4276c36fce7fa547583a
Elie Saliba <ersaliba@outlook.com>
2022-10-29 00:39:33 -0400

    Added post endpoint to accept and store new stores from the client

--- flask_practice/app.py
+++ flask_practice/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,3 +18,12 @@ stores = [
 @app.get("/store") # <-- Endpoint
 def get_stores(): # <-- Function associated with above endpoint
     return {"stores": stores}
+
+
+# creating an endpoint to receive data that the client sends and add it to the stored data
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201 # Status code 201 is the default OK good
