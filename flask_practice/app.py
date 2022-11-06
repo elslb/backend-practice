f64b2bee037793b40545c3e6f93ebbca773c537d
Elie Saliba <ersaliba@outlook.com>
2022-11-05 23:58:58 -0400

    Refactored get_store, get_stores and create_store to work with new data structure

--- flask_practice/app.py
+++ flask_practice/app.py
@@ -1,21 +1,25 @@
 from flask import Flask, request
 from db import items, stores
+import uuid
 
 app = Flask(__name__)
 
 # creating an endpoint to return the above data when the client requests it
 @app.get("/store") # <-- Endpoint
 def get_stores(): # <-- Function associated with above endpoint
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
+    # ^ needs to be cast into a list because stores.values behaves like a list but is not actually one
+    # this is in order for it to be turned into JSON
 
 
 # creating an endpoint to receive data that the client sends and add it to the stored data
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201 # Status code 201 is the default OK good
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex # will not need to do this when using an actual DB
+    store = {**store_data, "id": store_id} # **store_data passes that kwarg into this new dict and unpacks the values in it
+    stores[store_id] = store
+    return store, 201 # Status code 201 is the default OK good
 
 
 # creating an endpoint to receive dynamic paths and add items to the store
@@ -31,11 +35,11 @@ def create_item(name): # name comes from the url portion 'string:name'
 
 
 # endpoint to get a specific store by name
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
         return {"message": "Store not found!"}, 404
 
 
